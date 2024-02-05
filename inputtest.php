82ccf7e25bb59c838d1e68825ea74acb349a2b4e
Don <dontran2019@gmail.com>
2024-02-05 16:25:08 -0500

    test file for user inputs

---
+++ inputtest.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+
+</head>
+
+<title>testing</title>
+<body>
+<h1>don doing some testing</h1>
+
+
+<!-- Clean up. -->
+<?php
+        mysqli_free_result($result);
+        mysqli_close($connection);
+?>
+
+</body>
+</html>
+ 
