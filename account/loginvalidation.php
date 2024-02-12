d062918c36a9e01c81e5e1b68de1aec7cd7505d4
Don <donnamtran@pm.me>
2024-02-12 00:22:09 -0500

    Added the account_type session variable to track user account type.

--- account/loginvalidation.php
+++ account/loginvalidation.php
@@ -43,13 +43,15 @@
         
         //Checks if the username exists in the database.
         if (strcmp($query_data[1], "") != 0) {
-                $result = mysqli_query($connection, "SELECT * FROM $query_data[2]s WHERE username = '$name'");
+                $_SESSION['account_type'] = $query_data[2];
+                $query = "SELECT * FROM ".$_SESSION['account_type']."s WHERE username = '$name'";
+                $result = mysqli_query($connection, $query);
                 $resultE = mysqli_query($connection, "SELECT * FROM $query_data[2]s WHERE email = '$name'");
                 $query_data = mysqli_fetch_row($result);
                 $query_dataE = mysqli_fetch_row($resultE);
                 if(password_verify($password, $query_data[5]) || password_verify($password, $query_dataE[5])) {
                         $_SESSION['login'] = true;
-                        header("Location: http://team05sif.cpsc4911.com/S24-Team05/account/driverhomepage.php");
+                        header("Location: http://team05sif.cpsc4911.com/S24-Team05/account/".$_SESSION['account_type']."homepage.php");
                         exit();
                 } else {
                         $_SESSION['errors']['login'] = "Incorrect username or password!";
