788bfb2170a6943597b5e70b33741e96b2a21891
Don <dontran2019@gmail.com>
2024-02-21 16:49:55 -0500

    Created the file for the profile page and added a variable that stores the user's data for the session.

---
+++ account/profilepage.php
@@ -0,0 +1,10 @@
+<?php
+  session_start();
+  if(!$_SESSION['login']) {
+    echo "Invalid page.<br>";
+    echo "Redirecting.....";
+    sleep(2);
+    header( "Location: http://team05sif.cpsc4911.com/", true, 303);
+    exit();
+  }
+?>
