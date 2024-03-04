155b2808ce58b59bc1e3c724605e37d2566883ac
Jacarmichael712 <mrmafioso712@gmail.com>
2024-03-04 11:51:28 -0500

    James Carmichael: Fixed issue where point values were not being compared correctly for sorting (Tasks 542, 543, 544, 545, 546, 547, 548, 549, 551, 552)

--- points/sponsor_view_driver_points.php
+++ points/sponsor_view_driver_points.php
@@ -144,9 +144,9 @@ th {
 <div class="div_before_table">
 <table id="myTable2">
     <tr>
-        <th class="sticky" onclick="sortTable(0)">Driver First Name</th>
-        <th class="sticky" onclick="sortTable(1)">Driver Last Name</th>
-        <th class="sticky" onclick="sortTable(2)">Number of Points</th>
+        <th class="sticky" onclick="sortTableByText(0)">Driver First Name</th>
+        <th class="sticky" onclick="sortTableByText(1)">Driver Last Name</th>
+        <th class="sticky" onclick="sortTableByNumber(2)">Number of Points</th>
     </tr>
     <!-- PHP CODE TO FETCH DATA FROM ROWS -->
     <?php 
@@ -175,7 +175,7 @@ th {
 <!-- Javascript table sorting function sourced from W3Schools. Link to code in README -->
 <script type="text/javascript">
   // Sorting function for the table columns from W3Schools
-  function sortTable(n) {
+  function sortTableByText(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById("myTable2");
     switching = true;
@@ -206,6 +206,61 @@ th {
           }
         } else if (dir == "desc") {
           if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        // Each time a switch is done, increase this count by 1:
+        switchcount ++;
+      } else {
+        /* If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again. */
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
+
+  function sortTableByNumber(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("myTable2");
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /* Loop through all table rows (except the
+      first, which contains table headers): */
+      for (i = 1; i < (rows.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /* Check if the two rows should switch place,
+        based on the direction, asc or desc: */
+        if (dir == "asc") {
+          if (parseInt(x.innerHTML) > parseInt(y.innerHTML)) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
             // If so, mark as a switch and break the loop:
             shouldSwitch = true;
             break;
