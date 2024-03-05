0930ac112608eea9dcb8a6b549448263f24f930f
ubuntu <ubuntu@ip-172-31-42-20.ec2.internal>
2024-03-05 21:15:50 +0000

    Changed id to driver_id on various files -Don Tran

--- points/remove_points.php
+++ points/remove_points.php
@@ -184,7 +184,7 @@ th {
     <tr>
         <!-- FETCHING DATA FROM EACH
             ROW OF EVERY COLUMN -->
-        <td><?php echo $rows['id'];?></td>
+        <td><?php echo $rows['driver_id'];?></td>
         <td><?php echo $rows['driver_first_name'];?></td>
         <td><?php echo $rows['driver_last_name'];?></td>
     </tr>
