23f6dbf141b527aa34a996df868b2d1151011e7b
Jacarmichael712 <mrmafioso712@gmail.com>
2024-03-12 12:59:04 -0400

    James Carmichael: Cart testing (Tasks 51, 592, 611)

--- cart/add_to_cart.php
+++ cart/add_to_cart.php
@@ -206,6 +206,9 @@ input[type=submit]:hover {
 <body>
 
 <?php
+    ini_set('display_errors',1);
+    error_reporting(E_ALL);
+    
     session_start();
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
