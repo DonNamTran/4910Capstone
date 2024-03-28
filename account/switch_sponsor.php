6bab1b14f8a083b7b260a1328c0c7cf477bb4b63
Jacarmichael712 <mrmafioso712@gmail.com>
2024-03-28 18:19:12 -0400

    James Carmichael: Working on implementing a way for drivers to switch to different sponsors (Task IDS: 674, 670)

--- account/switch_sponsor.php
+++ account/switch_sponsor.php
@@ -17,14 +17,14 @@ $sponsor_name = $_POST['sponsor_name'];
 $driver_id = $_POST['driver_id'];
 
 $orgEntry = mysqli_query($conn, "SELECT assoc_points FROM driver_sponsor_assoc WHERE driver_id=$driver_id AND assoc_sponsor_id=$sponsor_id");
-echo("Before assocPoints");
-echo ($orgEntry->fetch_assoc())['assoc_points'];
 $assocPoints = ($orgEntry->fetch_assoc())['assoc_points'];
 
+echo("Before update sponsor query");
 $updateSponsorQuery = "UPDATE drivers SET associated_sponsor=?, driver_points=? WHERE driver_id=$driver_id";
 $updateSponsorSTMT = $conn->prepare($updateSponsorQuery);
 $updateSponsorSTMT->bind_param("si", $sponsor_name, $assocPoints);
 
+echo("Before execute");
 if ($updateSponsorSTMT->execute()) {
     echo '<script>alert("Successfully switched sponsors!\n")</script>';
     echo '<script>window.location.href = "http://team05sif.cpsc4911.com/S24-Team05/account/driverhomepage.php"</script>';
