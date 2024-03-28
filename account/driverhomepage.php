472df57a8f9ec4466fb38e2e7819f4ca5decb94e
Jacarmichael712 <mrmafioso712@gmail.com>
2024-03-28 16:33:44 -0400

    James Carmichael: Working on implementing a way for drivers to switch to different sponsors (Task IDS: 674, 670)

--- account/driverhomepage.php
+++ account/driverhomepage.php
@@ -195,7 +195,7 @@ input[type=submit] {
         } 
         
         $username = $_SESSION['username'];
-        $driver_id = mysqli_query($connection, "SELECT * FROM drivers WHERE driver_username = '$username' AND driver_archived=0");
+        $driver_id = mysqli_query($connection, "SELECT driver_id FROM drivers WHERE driver_username = '$username' AND driver_archived=0");
 
         echo("before assoc spons query");
         $assoc_spons_query = mysqli_query($connection, "SELECT * FROM driver_sponsor_assoc WHERE driver_id=$driver_id");
