d85af677d013f5cc8454d385d701c5709e6a8ded
Jacarmichael712 <mrmafioso712@gmail.com>
2024-03-28 18:12:40 -0400

    James Carmichael: Working on implementing a way for drivers to switch to different sponsors (Task IDS: 674, 670)

--- account/switch_sponsor.php
+++ account/switch_sponsor.php
@@ -21,7 +21,7 @@ echo("Before org entry");
 echo $sponsor_id;
 echo $sponsor_name;
 echo $driver_id;
-$orgEntry = mysqli_query($connection, "SELECT assoc_points FROM driver_sponsor_assoc WHERE driver_id=$driver_id AND assoc_sponsor_id=$sponsor_id");
+$orgEntry = mysqli_query($connection, "SELECT * FROM driver_sponsor_assoc WHERE driver_id=$driver_id AND assoc_sponsor_id=$sponsor_id");
 echo("Before assocPoints");
 $assocPoints = ($orgEntry->fetch_assoc())['assoc_points'];
 
