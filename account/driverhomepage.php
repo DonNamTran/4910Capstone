55cee155ee98b617ecfdb2eb6604c8af74f25ae8
Jacarmichael712 <mrmafioso712@gmail.com>
2024-03-28 17:52:29 -0400

    James Carmichael: Working on implementing a way for drivers to switch to different sponsors (Task IDS: 674, 670)

--- account/driverhomepage.php
+++ account/driverhomepage.php
@@ -208,7 +208,9 @@ input[type=submit] {
           $sponsor_name = ($sponsor_name->fetch_assoc())['organization_username'];
 
           echo("<form action='http://team05sif.cpsc4911.com/S24-Team05/account/switch_sponsor.php' method='post'>
+            input type='hidden' name='driver_id' value='$driver_id'/>
             <input type='hidden' name='sponsor_id' value='$sponsor_id'/>
+            <input type='hidden' name='sponsor_name' value='$sponsor_name'/>
             <input type='submit' class='link' value='$sponsor_name'/>
             </form>");
         }
