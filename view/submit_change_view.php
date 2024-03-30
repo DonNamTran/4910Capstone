98b89c8beba957b961d92edd4007fb0171a84e22
ubuntu <ubuntu@ip-172-31-42-20.ec2.internal>
2024-03-30 00:01:38 +0000

    Haley Wynne: Added comments

--- view/submit_change_view.php
+++ view/submit_change_view.php
@@ -4,17 +4,9 @@
   $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
   $database = mysqli_select_db($connection, DB_DATABASE);
 
-  /*if(!$_SESSION['login'] || !isset($_SESSION['user_edited']['query'])) {
-      echo "Invalid page.<br>";
-      echo "Redirecting.....";
-      sleep(2);
-      header( "Location: http://team05sif.cpsc4911.com/", true, 303);
-      exit();
-  }*/
-
   $result = mysqli_query($connection, "SELECT * FROM users");
 
-  // Get the driver associated sponsor for order table
+  // Get user id
   $username = $_SESSION['username'];
   while($rows=$result->fetch_assoc()) {
     if($rows['username'] == $username) {
@@ -22,6 +14,7 @@
     }
   }
 
+  // Reset session variable depending on view change
   if(isset($_POST['change_view'])) {
     if(strcmp($_POST['change_view'], "administrator") == 0) {
         $_SESSION['account_type'] = "administrator";
@@ -33,6 +26,7 @@
 
     $new_view = $_SESSION['account_type'];
 
+    // Update the view in the users table
     $queryOne = "UPDATE users SET user_view_type = '$new_view' WHERE id = '$user_id'";
     mysqli_query($connection, $queryOne);
     $_SESSION['errors']['user_info'] = "Information updated!";
