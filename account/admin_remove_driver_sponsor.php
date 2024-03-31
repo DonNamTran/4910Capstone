0c154f1cc8ab07b0ca15711ab4f1d6eeb8eadae2
Don <dontran2019@gmail.com>
2024-03-31 15:10:46 -0400

    admins can remove sponsor companies from driver's now.

--- account/admin_remove_driver_sponsor.php
+++ account/admin_remove_driver_sponsor.php
@@ -8,4 +8,20 @@
     $oragnization = $_POST['organization'];
     $sponsor_id = $_POST['sponsor_id'];
     echo $driver_id, $oragnization, $sponsor_id;
+
+
+    $sql_remove_sponsor = "DELETE FROM driver_sponsor_assoc WHERE driver_id=? AND assoc_sponsor_id=?";
+    $stmt_removed = $connection->prepare($sql_remove_sponsor);
+    $stmt_removed->bind_param('ii', $driver_id, $sponsor_id);
+    $stmt_removed->execute();
+
+    $sql_next_sponsor = "SELECT * FROM driver_sponsor_assoc CROSS JOIN organizations 
+    ON driver_sponsor_assoc.assoc_sponsor_id=organizations.organization_id WHERE driver_id=$driver_id;";
+    $result = mysqli_query($connection, $sql_next_sponsor);
+    $next_sponsor = ($result->fetch_assoc())['organization_username'];
+    if($next_sponsor) {
+        echo $next_sponsor;
+    } else {
+        echo 'None';
+    }
 ?>
