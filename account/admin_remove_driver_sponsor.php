eec570dad6620fdd0978ff85bfb5f259dcec789b
Don <dontran2019@gmail.com>
2024-03-31 14:31:10 -0400

    Added the file where sponsors will be removed.

---
+++ account/admin_remove_driver_sponsor.php
@@ -0,0 +1,11 @@
+<?php include "../../../inc/dbinfo.inc"; ?>
+<?php session_start();?>
+
+<?php 
+    $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+    $database = mysqli_select_db($connection, DB_DATABASE);
+    $driver_id = $_POST['driver_id'];
+    $oragnization = $_POST['organization'];
+    $sponsor_id = $_POST['sponsor_id'];
+    echo $driver_id, $oganization, $sponsor_id;
+?>
