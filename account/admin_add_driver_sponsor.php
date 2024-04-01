fcc9c0e30e158f09183d68995005af27ad4b1aac
Don <donnamtran@pm.me>
2024-04-01 04:14:33 -0400

    Minor bug fixes

--- account/admin_add_driver_sponsor.php
+++ account/admin_add_driver_sponsor.php
@@ -9,13 +9,14 @@
     $oragnization = $_POST['sponsor'];
     //echo $driver_id, $oragnization, $sponsor_id;
 
-    $get_sponsor_id = mysqli_query($connection, "SELECT * FROM organizations WHERE organization_username=$organization");
+    $get_sponsor_id = mysqli_query($connection, "SELECT * FROM organizations WHERE organization_username='$organization'");
     $sponsor_id = ($get_sponsor_id->fetch_assoc())['organization_id'];
     
     //Query to remove the driver-sponsor association.
     $sql_remove_sponsor = "INSERT INTO driver_sponsor_assoc (driver_id, driver_username, assoc_sponsor_id, assoc_points) VALUES (?,?,?,?);";
     $stmt_removed = $connection->prepare($sql_remove_sponsor);
-    $stmt_removed->bind_param('isii', $driver_id, $driver_name, $sponsor_id, 0);
+    $points = 0;
+    $stmt_removed->bind_param('isii', $driver_id, $driver_name, $sponsor_id, $points);
     if($stmt_removed->execute()) {
         $redirectpage = "admin_edit_driver_account.php";
         echo '<script>alert("Succesfully added sponsor company to driver!")</script>';
