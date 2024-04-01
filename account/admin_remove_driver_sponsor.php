c1b3554c46c9d85d4c1a476d3754b49a42183c4e
Don <donnamtran@pm.me>
2024-04-01 04:18:18 -0400

    Typo fixed

--- account/admin_remove_driver_sponsor.php
+++ account/admin_remove_driver_sponsor.php
@@ -5,7 +5,7 @@
     $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
     $database = mysqli_select_db($connection, DB_DATABASE);
     $driver_id = $_POST['driver_id'];
-    $oragnization = $_POST['organization'];
+    $organization = $_POST['organization'];
     $sponsor_id = $_POST['sponsor_id'];
     //echo $driver_id, $oragnization, $sponsor_id;
 
