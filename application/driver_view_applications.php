b1b6957d94bacb9cbb2bf52b8eb66d23cae28741
Jacarmichael712 <mrmafioso712@gmail.com>
2024-04-05 15:00:27 -0400

    James Carmichael: Adding ability for drivers to see the applications they've submitted and the status of the applications (Task IDs: 695, 696, 697, 698)

--- application/driver_view_applications.php
+++ application/driver_view_applications.php
@@ -229,6 +229,8 @@ th {
 <body>
 
 <?php
+    echo("Very start of file");
+
     ini_set('display_errors',1);
     error_reporting(E_ALL);
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
