fd04acb8473ba33f142b2888b12be6a5d34fc257
madduxr <madduxr@clemson.edu>
2024-04-08 21:59:40 -0400

    Maddux committing for driver app

--- application/submit_driver_apply.php
+++ application/submit_driver_apply.php
@@ -24,7 +24,7 @@ $query = mysqli_query($connection, "SELECT * FROM organizations WHERE organizati
     }
 $appStatus = "Pending";
 $comments = $_POST['comments'];
-$driver_id = $_SESSION['real_account_type'] . "_username";
+$driver_id = $_SESSION['real_account_type'] . "_id";
 
 
 
