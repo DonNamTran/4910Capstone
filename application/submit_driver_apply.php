16584f40da1ab0954111a690934e3a2e87348e4d
madduxr <madduxr@clemson.edu>
2024-04-08 21:23:54 -0400

    Maddux committing for driver app

--- application/submit_driver_apply.php
+++ application/submit_driver_apply.php
@@ -12,8 +12,21 @@ if (mysqli_connect_errno()) {
 
 // Get query variables from POST
 $sponsorApp = $_POST['listsponsors'];
+
+$connection = mysqli_connect(DB_SERVER, DB_USERNMAE, DB_PASSWORD, DB_DATABASE);
+$database = mysqli_select_db($connection, DB_DATABASE);
+
+$query = mysqli_query($connection, "SELECT * FROM organizations WHERE organization_archived=0");
+
+    while($rows=$query->fetch_assoc()) {
+        $orgId = $rows['organization_id'];
+    }
+$appStatus = "Peding";
 $comments = $_POST['comments'];
-$driver_id = $_SESSION['driver_id']
+$driver_id = $_SESSION['user_data'][$_SESSION['real_account_type']."_id"];
+
+
+
 $appDateTime = new DateTime('now');
 $appDate = $appDateTime->format('Y-m-d');
 $user_type = 'driver';
@@ -24,31 +37,18 @@ function validateDate($date, $format = 'Y-m-d'){
     return $d && $d->format($format) === $date;
 }
 
-// Check for invalid info
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-    echo '<script>alert("Invalid email address format!\n\nPlease enter in a valid email address and retry...")</script>';
-    echo '<script>window.location.href = "driver_apply.php"</script>';
-} else {
-    /*TO DO: Prepare query on application table
-    $sql_apply = "INSERT INTO applications (driver_first_name, driver_last_name, driver_email, driver_phone_number, driver_app_date, driver_applied_sponsor) VALUES (?, ?, ?, ?, ?, ?)";
+
+    $sql_apply = "INSERT INTO applications (driver_id, organization_id, application_status, application_date, application_comments) VALUES (?, ?, ?, ?, ?)";
     $stmt_apply = $conn->prepare($sql_apply);
-    $stmt_apply->bind_param("ssssss", $fname, $lname, $email, $password, $phone, $appDate, $appSponsor);
+    $stmt_apply->bind_param("iisss", $driver_id, $orgId, $appStatus, $appDate, $comments);
 
-    // Prepare query on users table
-    $sql_users = "INSERT INTO users (username, user_type, user_email, user_view_type) VALUES (?, ?, ?, ?)";
-    $stmt_users = $conn->prepare($sql_users);
-    $stmt_users->bind_param("ssss", $username, $user_type, $email, $user_view_type);
-
-    if ($stmt_drivers->execute() && $stmt_users->execute()) {
-        echo '<script>alert("Your account is ready!\n\nRedirecting to login page...")</script>';
-        echo '<script>window.location.href = "login.php"</script>';
+    if ($stmt_drivers->execute()) {
+        echo '<script>alert("Your application has been submitted!\n\nRedirecting to homepage...")</script>';
+        echo '<script>window.location.href = "http://team05sif.cpsc4911.com/S24-Team05/account/driverhomepage.php"</script>';
+    }else{
+        echo '<script>alert("Your application submission failed...\n\nCheck your information and retry...")</script>';
+        echo '<script>window.location.href = "driver_apply.php"</script>';
     }
-    else{
-        echo '<script>alert("Failed to create account...\n\nCheck your information and retry...")</script>';
-        echo '<script>window.location.href = "driver_account_creation.php"</script>';
-    }
-    */
-}
 ?>
 
 </body>
