2f2facf713e067da3fed87cbaa557539ce4b76d8
Don <donnamtran@pm.me>
2024-04-13 19:29:57 -0400

    Testing date picker

--- reporting/generate_sales_by_sponsor.php
+++ reporting/generate_sales_by_sponsor.php
@@ -13,7 +13,18 @@
 
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+  </script>
 <style type="text/css">
+
 body {
   background-color: #fff5d1;
   margin: 0;
@@ -220,7 +231,9 @@ input[type=submit]:hover {
           <?php } ?>   
         </select><br>
   <input type="submit" value="Submit"><br>
+  <p>Date: <input type="text" id="datepicker"></p>
 </form>
+
 
 </body>
 
