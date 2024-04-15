08d71063546c432f9027d406dabce20a92b85f9e
Jacarmichael712 <mrmafioso712@gmail.com>
2024-04-15 12:13:17 -0400

    James Carmichael: Testing php chart functionality

--- reporting/phpchart_test.php
+++ reporting/phpchart_test.php
@@ -5,13 +5,21 @@ require_once("../phpChart_Lite/conf.php");
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>phpChart - Basic Chart</title>
+<title>Sales by Sponsor</title>
 </head>
 <body>
    
 <?php
-$pc = new C_PhpChartX(array(array(11, 9, 5, 12, 14)),'basic_chart');
-$pc->draw();
+$line1 = array(array('Amazon', 3), array('Subway', 7), array('Microsoft', 2.5), array('Publix', 6),array('Walmart', 5),array('Wendys', 4));
+
+$pc = new C_PhpChartX(array($line1),'plot4');
+$pc->add_plugins(array('canvasTextRenderer'));
+$pc->set_title(array('text'=>'Sales by Sponsor'));
+$pc->set_animate(true);
+$pc->set_legend(array('show'=>true));
+$pc->set_series_default(array('renderer'=> 'plugin::PieRenderer', 'rendererOptions'=> array('sliceMargin'=>8)));
+
+$pc->draw(600,310);
 ?>
 
 </body>
