b19d68b026c1bd34d6e75432d10dd940e0d931fe
Jacarmichael712 <mrmafioso712@gmail.com>
2024-04-15 12:00:08 -0400

    James Carmichael: Testing php chart functionality

--- reporting/phpchart_test.php
+++ reporting/phpchart_test.php
@@ -5,7 +5,6 @@ require_once("../phpChart_Lite/conf.php");
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 <title>phpChart - Basic Chart</title>
 </head>
 <body>
