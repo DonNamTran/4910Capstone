c75bb4642a712c722202902efbe0d95e397e833d
Don <dontran2019@gmail.com>
2024-04-15 15:10:16 -0400

    Testing admin being able to select driver.

--- reporting/admin_generate_reports.php
+++ reporting/admin_generate_reports.php
@@ -209,11 +209,11 @@ input[type=submit]:hover {
   <input type="submit" class="link" value="Sales By Sponsor" />
 </form>
 
-<form action="http://team05sif.cpsc4911.com/S24-Team05/reporting/admin_sales_by_driver.php">
+<form action="http://team05sif.cpsc4911.com/S24-Team05/reporting/admin_select_driver_id.php">
   <input type="submit" class="link" value="Sales By Driver" />
 </form>
 
-<form action="http://team05sif.cpsc4911.com/S24-Team05/reporting/admin_enter_driver_id.php">
+<form action="http://team05sif.cpsc4911.com/S24-Team05/reporting/admin_select_driver_id.php">
   <input type="submit" class="link" value="Invoice" />
 </form>
 
