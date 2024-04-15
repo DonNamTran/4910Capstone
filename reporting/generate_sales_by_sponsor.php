b2ff22f1dd1bd31bf1d8ff849ff9e5fe1f6946bc
Don <donnamtran@pm.me>
2024-04-15 02:50:39 -0400

    typo fixes

--- reporting/generate_sales_by_sponsor.php
+++ reporting/generate_sales_by_sponsor.php
@@ -28,14 +28,14 @@
     JOIN organizations 
 		ON orders.order_associated_sponsor=organizations.organization_username WHERE order_associated_sponsor='Subway' 
             AND order_contents_removed = 0 AND order_date_ordered BETWEEN '$start_range' AND '$end_range_format'
-        GROUP BY order_contents_item_type"
+        GROUP BY order_contents_item_type";
     $total_by_item = mysqli_query($connection, $total_sponsor_sales_by_item_query);
     while($row=$total_by_item->fetch_assoc()) {
         $sales_by_item =  number_format($row['total_sales'], 2);
-        echo "$row['order_contents_item_type']s have generated $$sales_by_item between $strang_range and $end_range for $sponsor <br>"
+        echo "$row['order_contents_item_type']s have generated $$sales_by_item between $strang_range and $end_range for $sponsor <br>";
     }
 
-    echo "$sponsor has generated $$total_sales between $start_range and $end_range";
+    echo "$sponsor has generated $$total_sales between $start_range and $end_range <br>";
     //var_dump($result);
 
     //$stmt_total_sales = $connection->prepare($total_sponsor_sales_query);
