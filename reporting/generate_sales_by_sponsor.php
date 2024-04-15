3ebcafac57c28d8a2f53cd0d98a5e7820340bd07
Don <donnamtran@pm.me>
2024-04-14 22:05:19 -0400

    small typo fixed

--- reporting/generate_sales_by_sponsor.php
+++ reporting/generate_sales_by_sponsor.php
@@ -9,14 +9,14 @@
 
     $end_range_format = new DateTime($end_range);;
     $end_range_format->add(new DateInterval("PT23H59M59S"));
-    $end_range = $end_range_format->format("Y-m-d H:i:s");;
+    $end_range_format = $end_range_format->format("Y-m-d H:i:s");;
 
     $total_sponsor_sales_query = "SELECT *, SUM(order_contents_item_cost)*organization_dollar2pt AS total_sales FROM orders 
 	JOIN order_contents 
 		ON orders.order_id = order_contents.order_id
     JOIN organizations 
 		ON orders.order_associated_sponsor=organizations.organization_username 
-            WHERE order_associated_sponsor='$sponsor' AND order_contents_removed = 0 AND order_date_ordered BETWEEN '$start_range' AND '$end_range'
+            WHERE order_associated_sponsor='$sponsor' AND order_contents_removed = 0 AND order_date_ordered BETWEEN '$start_range' AND '$end_range_format'
         GROUP BY order_associated_sponsor";
     $total_sales = mysqli_query($connection, $total_sponsor_sales_query);
     $result = $total_sales->fetch_assoc();
