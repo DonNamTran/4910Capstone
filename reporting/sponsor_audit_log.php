3d6d6b954ee42ee5349df267cbc4886d12375fc9
ubuntu <ubuntu@ip-172-31-42-20.ec2.internal>
2024-04-18 20:41:20 +0000

    Fixed small issue

--- reporting/sponsor_audit_log.php
+++ reporting/sponsor_audit_log.php
@@ -1,13 +1,5 @@
-<?php
+<?php include "../../../inc/dbinfo.inc";
   session_start();
-  if(!$_SESSION['login'] || strcmp($_SESSION['account_type'], "sponsor") != 0) {
-    echo "Invalid page.<br>";
-    echo "Redirecting.....";
-    sleep(2);
-    header( "Location: http://team05sif.cpsc4911.com/", true, 303);
-    exit();
-    //unset($_SESSION['login']);
-  }
 ?>
 
 <html>
