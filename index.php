42c154d58a05b9308d750d245105204c452e72f3
Jacarmichael712 <mrmafioso712@gmail.com>
2024-04-18 12:49:07 -0400

    James Carmichael: Removed home from account creation nav bars because it broke it they weren't logged in

--- index.php
+++ index.php
@@ -239,7 +239,7 @@ form {
 
 <div class="wrapper">
   <div class="content">
-    <img src="S24-Team05/images/Logo.png" style="width: 750px; height: 500px;">
+    <img src="S24-Team05/images/Logo.png" style="width: 600px; height: 350px;">
   </div>
 </div>
   
