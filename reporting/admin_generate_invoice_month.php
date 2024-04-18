666ad05ad4f15f07f81e2ccbeb0157a08985475e
Maddux Rhodes <madduxrhodes@gmail.com>
2024-04-18 16:13:57 -0400

    Maddux commiting for invoicing single month

--- reporting/admin_generate_invoice_month.php
+++ reporting/admin_generate_invoice_month.php
@@ -19,8 +19,14 @@
 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
 <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
 <script>
+  var currentTime = new Date();
+  var minDate = new Date(currentTime.getFullYear(), currentTime.getMonth(), +1);
+  var maxDate = new Date(currentTime.getFullYear(), currentTime.getMonth() + 2, +0);
       $( function() {
-      $( ".datepicker" ).datepicker();
+      $( ".datepicker" ).datepicker(){
+        minDate: minDate,
+        maxDate: maxDate
+      }
                     } );
    </script>
 <style type="text/css">
@@ -223,8 +229,7 @@ input[type=submit]:hover {
   <input type="text" name="start_date" class="datepicker" required><br>
   <label for="end_date">Ending Date:</label><br>
   <input type="text" name="end_date" class="datepicker" required><br>
-  <input type="submit" value="Generate Summary Report"><br>
-  <input type="submit" formaction="http://team05sif.cpsc4911.com/S24-Team05/reporting/generate_sales_by_driver_detailed.php" value="Generate Detailed Report"><br>
+  <input type="submit" value="Generate Invoice"><br>
 
 </form>
 
