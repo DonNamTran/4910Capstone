08da68cd421a8c0355c621e6f0eec7bd0c4ea9ae
Maddux Rhodes <madduxrhodes@gmail.com>
2024-04-18 16:37:01 -0400

    Maddux commiting for invoicing single month

--- reporting/submit_admin_generate_invoice_month.php
+++ reporting/submit_admin_generate_invoice_month.php
@@ -208,15 +208,17 @@ input[type=submit]:hover {
   </div>
 </div>
 
-<div class="body">
+
 <body>
+<div class="body">
     <?php 
         $startDate = $_POST['start_date'];
         echo $startDate;
         $start_range = (new DateTime($start_range))->format("Y-m-d");
         $end_range = strtotime("+1 month", strtotime($start_range));
+        $end_range = strtotime("-17 days", strtotime($end_range));
         $end_range = date('m-d-Y', $end_range);
         echo $end_range;
     ?>
-</body>
 </div>
+</body>
