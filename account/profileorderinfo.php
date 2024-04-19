a1b67e9ad8f179d22039d7d775a6ee18ecc6bd4a
ubuntu <ubuntu@ip-172-31-42-20.ec2.internal>
2024-04-19 17:01:04 +0000

    Removed switch sponsor from navbar on some pages

--- account/profileorderinfo.php
+++ account/profileorderinfo.php
@@ -205,6 +205,8 @@ p {
     <a href="/S24-Team05/account/profileuserinfo.php">Profile</a>
     <a href="/S24-Team05/account/logout.php">Logout</a>
     <a href="/S24-Team05/about_page.php">About</a>
+    <?php if($curr_sponsor != "none") {?> <a href="/S24-Team05/catalog/catalog_home.php">Catalog</a> <?php } ?>
+    <?php if($curr_sponsor != "none") {?> <a href="/S24-Team05/order/order_history.php">Orders</a> <?php } ?>
   </div>
 </div>
 
