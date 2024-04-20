0d0a883a2384b24996426d47798c638dd14f863a
Jacarmichael712 <mrmafioso712@gmail.com>
2024-04-20 11:56:00 -0400

    James Carmichael: Adding ability for sponsors to buy items on behalf of drivers

--- catalog/sponsor_buy_item_for_driver.php
+++ catalog/sponsor_buy_item_for_driver.php
@@ -287,6 +287,8 @@ th {
 
     $item_image = base64_encode(file_get_contents($image_data));
 
+    $username = $_SESSION['username'];
+
     // Check whether account is admin viewing as sponsor or is an actual sponsor account
     if(strcmp($_SESSION['account_type'], $_SESSION['real_account_type']) == 0) {
         $result = mysqli_query($connection, "SELECT * FROM sponsors");
@@ -310,21 +312,15 @@ th {
         }
         }
 
-    echo "before sponsor id";
     $sponsor_id = mysqli_query($connection, "SELECT * FROM organizations WHERE organization_username='$sponsor_name'");
     $sponsor_id = ($sponsor_id->fetch_assoc())['organization_id'];
 
-    echo $sponsor_name;
-    echo $sponsor_id;
-    echo "before result 2";
     $result2 = mysqli_query($connection, "SELECT * FROM driver_sponsor_assoc 
                                           JOIN drivers ON drivers.driver_id = driver_sponsor_assoc.driver_id
                                           WHERE assoc_sponsor_id=$sponsor_id 
                                           AND driver_sponsor_assoc_archived=0 
                                           AND driver_archived=0
                                           ORDER BY drivers.driver_username");
-
-    echo "after result 2";
 ?>
 <div class = "grid-container">
     <div class = "item">
