aca0d4388a1d88212d0d20e0d6c959b27b432893
ubuntu <ubuntu@ip-172-31-42-20.ec2.internal>
2024-04-20 18:02:17 +0000

    Fixed switch sponsor to work with different views

--- account/switch_sponsor.php
+++ account/switch_sponsor.php
@@ -12,28 +12,34 @@ if (mysqli_connect_errno()) {
     echo "Database connection failed.";  
 }  
 
-$sponsor_id = $_POST['sponsor_id'];
-$sponsor_name = $_POST['sponsor_name'];
-$driver_id = $_POST['driver_id'];
+if(strcmp($_SESSION['real_account_type'], "driver") == 0) {
+    $sponsor_id = $_POST['sponsor_id'];
+    $sponsor_name = $_POST['sponsor_name'];
+    $driver_id = $_POST['driver_id'];
 
-$orgEntry = mysqli_query($conn, "SELECT assoc_points
-                                 FROM driver_sponsor_assoc 
-                                 WHERE driver_id=$driver_id 
-                                 AND assoc_sponsor_id=$sponsor_id");
-$assocPoints = ($orgEntry->fetch_assoc())['assoc_points'];
+    $orgEntry = mysqli_query($conn, "SELECT assoc_points
+                                    FROM driver_sponsor_assoc 
+                                    WHERE driver_id=$driver_id 
+                                    AND assoc_sponsor_id=$sponsor_id");
+    $assocPoints = ($orgEntry->fetch_assoc())['assoc_points'];
 
-$updateSponsorQuery = "UPDATE drivers 
-                       SET driver_associated_sponsor=?, driver_points=? 
-                       WHERE driver_id=$driver_id";
-$updateSponsorSTMT = $conn->prepare($updateSponsorQuery);
-$updateSponsorSTMT->bind_param("si", $sponsor_name, $assocPoints);
+    $updateSponsorQuery = "UPDATE drivers 
+                        SET driver_associated_sponsor=?, driver_points=? 
+                        WHERE driver_id=$driver_id";
+    $updateSponsorSTMT = $conn->prepare($updateSponsorQuery);
+    $updateSponsorSTMT->bind_param("si", $sponsor_name, $assocPoints);
 
-if ($updateSponsorSTMT->execute()) {
-    echo '<script>alert("Successfully switched sponsors!\n")</script>';
-    echo '<script>window.location.href = "http://team05sif.cpsc4911.com/S24-Team05/account/driverhomepage.php"</script>';
+    if ($updateSponsorSTMT->execute()) {
+        echo '<script>alert("Successfully switched sponsors!\n")</script>';
+        echo '<script>window.location.href = "http://team05sif.cpsc4911.com/S24-Team05/account/driverhomepage.php"</script>';
+    }
+    else{
+        echo '<script>alert("Failed to switch sponsors...")</script>';
+        echo '<script>window.location.href = "http://team05sif.cpsc4911.com/S24-Team05/account/driverhomepage.php"</script>';
+    }
 }
-else{
-    echo '<script>alert("Failed to switch sponsors...")</script>';
+else {
+    echo '<script>alert("Successfully switched sponsors!\n")</script>';
     echo '<script>window.location.href = "http://team05sif.cpsc4911.com/S24-Team05/account/driverhomepage.php"</script>';
 }
 ?>
