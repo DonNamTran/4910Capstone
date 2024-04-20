af25ae78cc5f67b0f8e19bfad6cb5bc1e6f4b285
Haley Wynne <hwynne@clemson.edu>
2024-04-20 16:09:26 -0400

    About page change

--- about_page.php
+++ about_page.php
@@ -193,8 +193,6 @@ form {
 <div class="navbar">
   <div class="menu">
     <a href="/">Landing Page</a>
-    <a href="/S24-Team05/about_page.php">About</a>
-    <a href="/S24-Team05/account/login.php">Login</a>
   </div>
 </div>
 
